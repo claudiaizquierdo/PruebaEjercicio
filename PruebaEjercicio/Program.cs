3d0068ae52271ca176ce1bdad521955c45632219
Claudia Izquierdo <claudiaizq82@hotmail.com>
2020-03-17 19:31:02 -0300

    Hola Mundo

--- PruebaEjercicio/Program.cs
+++ PruebaEjercicio/Program.cs
@@ -10,6 +10,8 @@ namespace PruebaEjercicio
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hola Mundo");
+            Console.ReadLine();
         }
     }
 }
